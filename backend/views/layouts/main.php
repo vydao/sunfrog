0009cf3dc8dfb066373264fc12c3ec047cd0c60a
Quyen <nguyenngocquyen110@gmail.com>
2015-04-20 21:55:22 +0700

    manage category

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -60,8 +60,8 @@ AppAsset::register($this);
 
     <footer class="footer">
         <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
-        <p class="pull-right"><?= Yii::powered() ?></p>
+        <p class="pull-left">&copy; Software Solutions</p>
+        <p class="pull-right"></p>
         </div>
     </footer>
 
---
+++ common/models/Category.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "category".
+ *
+ * @property integer $id
+ * @property string $name
+ * @property integer $priority
+ */
+class Category extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'category';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['name'], 'required'],
+            [['priority'], 'integer'],
+            [['name'], 'string', 'max' => 255]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'priority' => 'Priority',
+        ];
+    }
+}
