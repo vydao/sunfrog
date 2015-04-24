c2eab3051fe4ed2ad605f6241a1b074dd380f6be
QuyenNguyen <quyen.nguyen@namlongsoft.com>
2015-04-24 14:05:52 +0700

    update news

--- backend/web/js/admin.js
+++ backend/web/js/admin.js
@@ -7,7 +7,20 @@ $(document).ready(function(){
 			editor.on('change', function(e) {
 				$('textarea#news-content').val(tinymce.get('news-content').getContent());
 			});
-		}
+		},		
+        plugins: [
+            "advlist autolink lists link image charmap print preview hr anchor pagebreak",
+            "searchreplace wordcount visualblocks visualchars code fullscreen",
+            "insertdatetime media nonbreaking save table contextmenu directionality",
+            "emoticons template paste textcolor colorpicker textpattern"
+        ],
+        toolbar1: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image filemanager",
+        toolbar2: "print preview media | forecolor backcolor emoticons",
+        image_advtab: true,
+        templates: [
+            {title: 'Test template 1', content: 'Test 1'},
+            {title: 'Test template 2', content: 'Test 2'}
+        ]
 	});
 	tinymce.init({
         selector: ".tinymce_tag textarea",
