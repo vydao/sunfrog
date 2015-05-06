4fe68e4e864585a9a76a34a4d7cbd86afe7bbc50
Vy Dao <vynguyendao@gmail.com>
2015-05-06 21:28:18 +0700

    add category view

---
+++ frontend/views/site/_items.php
@@ -0,0 +1,22 @@
+<article class="content">
+    <?php if(!empty($products)){
+            $i = 0;
+    ?>
+      <?php foreach($products as $key => $product){
+              $i++;
+      ?>
+        <div class="product_info">
+            <a href="<?php echo Yii::$app->urlManager->createAbsoluteUrl(['detail/' . $product->id]) ?>" class="product_img">
+            <img src="<?php echo Yii::$app->request->baseUrl . "/img/{$product->image}"; ?>" border="0" /></a>
+            <h2><a href="" class="product_name"><?php echo $product->name; ?></a></h2>
+            <h3 class="product_price">$19.99</h3>
+        </div>
+        <?php if(4 == $i){
+                $i = 0;
+        ?>
+                <div class="product_space"></div>
+        <?php } ?>
+      <?php } ?>
+      <?php }else{ echo "<h1>Coming soon.</h1>"; } ?>
+    <div class="clear"></div>
+</article>
