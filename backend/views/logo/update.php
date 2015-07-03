6c9194ad9a42ebbc864fc6170bf9a9530f6ee992
Quyen Nguyen <nguyenngocquyen110@gmail.com>
2015-07-03 18:14:40 +0700

    update

--- backend/views/logo/update.php
+++ backend/views/logo/update.php
@@ -5,7 +5,7 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model common\models\Logo */
 
-$this->title = 'Update Logo: ' . ' ' . $model->id;
+$this->title = 'Update Logo';
 $this->params['breadcrumbs'][] = ['label' => 'Logos', 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = 'Update';
